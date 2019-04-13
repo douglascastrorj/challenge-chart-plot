3aa6222e1bc077f1ba38ad6351b0ca0ac650d3d4
Douglas Castro da Silva <douglascastrorj@gmail.com>
2019-04-12 22:00:02 -0300

    adicionando graficos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,66 @@ import React, { Component } from 'react';
 import './App.css';
 
 import CodeEditor from './components/CodeEditor';
+import Resizable from 'react-resizable-box';
+import Chart from 'react-apexcharts'
 
 class App extends Component {
+
+  state = {
+    height: 500,
+    text: "{type:'start', timestamp: '1231241251125'}",
+
+    chart: {
+      options: {
+        chart: {
+          id: 'apexchart-example'
+        },
+        xaxis: {
+          categories: ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Out']
+        }
+      },
+      series: [{
+        name: 'group-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      },
+      {
+        name: 'group-2',
+        data: [21, 33, 42, 44, 49, 21, 44, 53]
+      }]
+    }
+  }
+
+
+  codeRef = React.createRef();
+
+  onResize = (event, direction, ref, delta) => {
+    this.setState({ height: ref.style.height });
+    this.codeRef.current.editor.resize()
+  };
+
+  onChange = (text) => {
+    this.setState({ text });
+  };
+
   render() {
     return (
       <div className="App">
-       <CodeEditor />
+
+        <Resizable
+          width='100%'
+          height={this.state.height}
+          onResize={this.onResize}
+        >
+          <CodeEditor
+            height={this.state.height}
+            ref={this.codeRef}
+            text={this.state.text}
+            onChange={this.onChange}
+          />
+
+        </Resizable>
+
+        <Chart options={this.state.chart.options} series={this.state.chart.series} type="line" width={600} height={320} />
       </div>
     );
   }
--- src/components/CodeEditor/index.jsx
+++ src/components/CodeEditor/index.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import brace from 'brace';
 import AceEditor from 'react-ace';
 
 import 'brace/mode/javascript';
 import 'brace/theme/monokai';
 
-
-const CodeEditor = (props) => (
+const CodeEditor = React.forwardRef((props, ref) => (
     <AceEditor
-        style={{width: '100%'}}
+        ref={ref}
+        style={{width: '100%', height: props.height}}
         placeholder="Placeholder Text"
         mode="javascript"
         theme="monokai"
-        name="blah2"
+        name="editor"
         onLoad={props.onLoad}
         onChange={props.onChange}
         fontSize={14}
         showPrintMargin={true}
         showGutter={true}
         highlightActiveLine={true}
-        value={`function onLoad(editor) {
-        console.log("i've loaded");
-        }`}
+        value={props.text}
         setOptions={{
-        enableBasicAutocompletion: false,
-        enableLiveAutocompletion: false,
-        enableSnippets: false,
-        showLineNumbers: true,
-        tabSize: 2,
-        }}/>       
-);
+            enableBasicAutocompletion: false,
+            enableLiveAutocompletion: false,
+            enableSnippets: false,
+            showLineNumbers: true,
+            tabSize: 2,
+        }}
+    />       
+));
 
 export default CodeEditor;
